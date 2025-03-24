3066c2421e9463ede0fe5725e307e8fba6b5007c
Aldo Solares <aldo_solares@outlook.com>
2025-03-23 19:54:55 -0600

    Delete Introducción a Python/Códigos del curso directory

--- Introducción a Python/Códigos del curso/08.py
+++
@@ -1,8 +0,0 @@
-# Archivos
-mi_archivo = open("Prueba_2.txt", "r")
-
-# mi_archivo.writelines(["Hola nenis\n", "Hola nenis 2\n","Hola nenis 3\n"])
-
-archivo = mi_archivo.readlines()
-
-print(archivo)
